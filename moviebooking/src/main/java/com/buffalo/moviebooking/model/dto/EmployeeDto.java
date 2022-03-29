f791cfc618af23feb1bedff76d0f57f252bb0572
Phanidhar Reddy <phani@Phanidhars-MacBook-Pro.local>
2022-03-29 11:44:58 +0530

    Employee Dto addition

--- moviebooking/src/main/java/com/buffalo/moviebooking/model/dto/EmployeeDto.java
+++ moviebooking/src/main/java/com/buffalo/moviebooking/model/dto/EmployeeDto.java
@@ -1,4 +1,14 @@
 package com.buffalo.moviebooking.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeDto {
+    private Integer employeeId;
+    private String username;
+    private String password;
 }
